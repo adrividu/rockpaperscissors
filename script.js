2554399c81588196be7048fffc8989807d0dcf9a
adrividu <adrijaysimpson@gmail.com>
2022-02-12 17:55:48 +0100

    secondver

--- script.js
+++ script.js
@@ -21,21 +21,47 @@ function computerPlay(){
 
 }
 
-let playerSelection= 'Rock';
+//let playerSelection= 'Rock';
+function playerSelection(){
+    let seleccionUsuario = prompt('Rock, paper or scissors?')
 
-function playRound (playerSelection, computerSelection) {
+    return seleccionUsuario
+}
 
-    playerSelection = playerSelection.toUpperCase();
+function playRound (eleccionJugador, computerSelection) {
+
+   // console.log(eleccionJugador + 'aqui');
+    eleccionJugador = eleccionJugador.toUpperCase();
     computerSelection = computerSelection.toUpperCase();
 
-    if (playerSelection === 'ROCK'){
+    if (eleccionJugador === 'ROCK'){
         if (computerSelection === 'SCISSORS'){
-        return 'Rock Wins! Player Wins'
+        return 'You Win! Player Wins'
         } else if (computerSelection === 'ROCK') {
         return 'Tie marker! No one wins!'
         } else if (computerSelection === 'PAPER') {
-            return 'Paper wins! You lose!'
-        }
+            return 'Machine wins! You lose!'
+        } 
+    }
+
+    if (eleccionJugador === 'SCISSORS'){
+        if (computerSelection === 'ROCK'){
+        return 'Machine wins! You lose!'
+        } else if (computerSelection === 'SCISSORS') {
+        return 'Tie marker! No one wins!'
+        } else if (computerSelection === 'PAPER') {
+            return 'You Win! Player Wins'
+        } 
+    }
+
+    if (eleccionJugador === 'PAPER'){
+        if (computerSelection === 'SCISSORS'){
+        return 'Machine wins! You lose!'
+        } else if (computerSelection === 'ROCK') {
+        return 'You Win! Player Wins'
+        } else if (computerSelection === 'PAPER') {
+            return 'Tie marker! No one wins!'
+        } 
     }
     
 
@@ -43,16 +69,22 @@ function playRound (playerSelection, computerSelection) {
 
 }
 
-console.log(playRound(playerSelection,computerPlay()))
+//console.log(playRound(playerSelection,computerPlay()))
 
 function game(){
-    let marcadorJugador
-    let marcadorMaquina
-    for(let i=0;i<5;++i){
-        if(playRound() === 'Rock Wins! Player Wins'){
+    let marcadorJugador = 0;
+    let marcadorMaquina = 0;
+    for(let i=0;i<5;i++){
+        let jugada=playRound(playerSelection(),computerPlay());
+        if(jugada === 'You Win! Player Wins'){
             marcadorJugador++;
-        } else if (playRound() === 'Paper wins! You lose!') {
+        } else if (jugada === 'Machine wins! You lose!') {
             marcadorMaquina++;    
         }
     }
+
+    console.log('The final score: Player ' + marcadorJugador + '  Machine ' + marcadorMaquina)
 }
+
+
+game();
